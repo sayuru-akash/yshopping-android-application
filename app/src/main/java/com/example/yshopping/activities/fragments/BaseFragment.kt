50f8b0a93c36ca107c2ab36f77f3971b87007231
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2022-05-20 17:37:27 +0530

    Create BaseFragment.kt

---
+++ app/src/main/java/com/example/yshopping/activities/fragments/BaseFragment.kt
@@ -0,0 +1,40 @@
+package com.example.yshopping.activities.fragments
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.yshopping.R
+
+open class BaseFragment : Fragment() {
+
+    private lateinit var mProgressDialog: Dialog
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_base, container, false)
+    }
+
+    fun showProgressDialog() {
+        mProgressDialog = Dialog(requireActivity())
+        mProgressDialog.setContentView(R.layout.dialog_progress)
+        mProgressDialog.setCancelable(false)
+        mProgressDialog.setCanceledOnTouchOutside(false)
+        mProgressDialog.show()
+    }
+
+    fun hideProgressDialog() {
+        if (mProgressDialog.isShowing) {
+            mProgressDialog.dismiss()
+        }
+    }
+}
