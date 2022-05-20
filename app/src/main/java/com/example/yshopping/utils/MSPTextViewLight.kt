bcec9a725604ff832a0eae02181d164ce7609546
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2022-05-20 17:02:40 +0530

    Create MSPTextViewLight.kt

---
+++ app/src/main/java/com/example/yshopping/utils/MSPTextViewLight.kt
@@ -0,0 +1,17 @@
+package com.example.yshopping.utils
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class MSPTextViewLight (context: Context, attrs: AttributeSet):AppCompatTextView(context, attrs) {
+    init {
+        applyFont()
+    }
+
+    private fun applyFont(){
+        val typeface: Typeface = Typeface.createFromAsset(context.assets, "fonts/proxima_light.ttf")
+        super.setTypeface(typeface)
+    }
+}
